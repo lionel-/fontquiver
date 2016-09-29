bf34d983e2825b0ac8b9064d1ebc1b9d20c6b5f3
dgohel <david.gohel@ardata.fr>
2016-09-29 21:50:44 +0200

    add font_desc for gdtools

---
+++ R/font_desc.R
@@ -0,0 +1,183 @@
+#' @export
+#' @title font description to be used by graphical devices
+#' @format an object that describes a font
+#' @description Summarize a font package.
+#' @field get_data get a data.frame describing a font
+#' @field get_families get a list to be used by a graphic device.
+#' @field set_data convenience function to be used when creating a
+#' font description object.
+#' @details
+#' \code{font_desc} is only there to provide common structure and behavior
+#' for a given font description.
+#'
+#' @examples
+#' font_desc_bitstream_vera$new()$get_families()
+#' font_desc_bitstream_vera$new()$get_data()
+#' font_desc_dejavu$new()$get_families()
+#' font_desc_dejavu$new()$get_data()
+#' font_desc_liberation$new()$get_families()
+#' font_desc_liberation$new()$get_data()
+#' @importFrom R6 R6Class
+font_desc <- R6Class(
+  "font_desc",
+  public = list(
+
+    get_data = function(){
+      private$content
+    },
+    set_data = function(df){
+
+      stopifnot( "family" %in% names(df) )
+      stopifnot( "style" %in% names(df) )
+      stopifnot( "bold" %in% names(df) )
+      stopifnot( "italic" %in% names(df) )
+      stopifnot( "base" %in% names(df) )
+
+      stopifnot( all( c("sans", "serif", "mono") %in% df$family ) )
+      stopifnot( all( c("regular", "bold", "italic", "bolditalic") %in% df$style ) )
+
+      private$content <- df
+    },
+
+    get_families = function() {
+      families_variations <- c("sans", "serif", "mono")
+      stopifnot(all( families_variations %in% private$content$family ))
+
+      ldata <- split(private$content, private$content$family)[families_variations]
+      lapply(ldata, function(x) {
+        ref_result <- setNames( rep(NA, 4), c("regular", "bold", "italic", "bolditalic") )
+        existings <- intersect( x$style, names(ref_result) )
+        id <- match( existings, names(ref_result) )
+        ttf_files <- paste0(x$base[id], ".ttf")
+        y <- system.file( file.path("fonts", private$fontdir), package = private$package )
+        y <- paste0(y, "/", ttf_files)
+        y <- ifelse( file.exists(y), y, NA )
+        ref_result[id] <- y
+        ref_result
+      })
+    }
+  ),
+  private = list(
+    content = NULL,
+    fontdir = NULL,
+    package = NULL
+  )
+)
+
+#' @export
+#' @rdname font_desc
+#' @details
+#'
+#' \code{font_desc_bitstream_vera} return details about Bitstream Vera
+#' font families and styles.
+font_desc_bitstream_vera <- R6Class(
+  "font_bitstream_vera_spec",
+  inherit = font_desc,
+  public = list(
+    initialize = function() {
+
+      self$set_data ( data.frame(
+        family = rep( c( "sans", "serif", "mono" ), each = 4 ),
+        style = rep(c( "regular", "bold", "italic", "bolditalic"), 3 ),
+        bold = rep(c(FALSE, TRUE, FALSE, TRUE), 3 ),
+        italic = rep(c(FALSE, FALSE, TRUE, TRUE), 3 ),
+        base = c( "Vera", "VeraBd", "VeraIt", "VeraBI",
+                  "VeraSe", "VeraSeBd", NA, NA,
+                  "VeraMono", "VeraMoBd", "VeraMoIt", "VeraMoBI"),
+        stringsAsFactors = FALSE
+      ) )
+
+      private$fontdir <- "bitstream-vera-fonts"
+      private$package <- "fontBitstreamVera"
+    }
+  )
+)
+
+#' @export
+#' @rdname font_desc
+#' @details
+#'
+#' \code{font_desc_dejavu} return details about DejaVu
+#' font families and styles.
+font_desc_dejavu <- R6Class(
+  "font_desc_dejavu",
+  inherit = font_desc,
+  public = list(
+    initialize = function() {
+
+      self$set_data ( data.frame(
+        family = c("sans", "sans", "sans", "sans", "sans", "sanscondensed", "sanscondensed",
+                   "sanscondensed", "sanscondensed", "mono", "mono", "mono",
+                   "mono", "serif", "serif", "serif", "serif", "serifcondensed",
+                   "serifcondensed", "serifcondensed", "serifcondensed", "mathtexgyre"
+        ),
+        style = c("regular", "bold", "italic", "bolditalic", "extralight", "regular",
+                  "bold", "italic", "bolditalic", "regular", "bold", "italic",
+                  "bolditalic", "regular", "bold", "italic", "bolditalic", "regular",
+                  "bold", "italic", "bolditalic", "regular"),
+        bold = c(FALSE, TRUE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE,
+                 FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE,
+                 FALSE, TRUE, FALSE),
+        italic = c(FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE,
+                   FALSE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE,
+                   TRUE, TRUE, FALSE),
+        base = c("DejaVuSans", "DejaVuSans-Bold", "DejaVuSans-Oblique", "DejaVuSans-BoldOblique",
+                 "DejaVuSans-ExtraLight", "DejaVuSansCondensed", "DejaVuSansCondensed-Bold",
+                 "DejaVuSansCondensed-Oblique", "DejaVuSansCondensed-BoldOblique",
+                 "DejaVuSansMono", "DejaVuSansMono-Bold", "DejaVuSansMono-Oblique",
+                 "DejaVuSansMono-BoldOblique", "DejaVuSerif", "DejaVuSerif-Bold",
+                 "DejaVuSerif-Italic", "DejaVuSerif-BoldItalic", "DejaVuSerifCondensed",
+                 "DejaVuSerifCondensed-Bold", "DejaVuSerifCondensed-Italic", "DejaVuSerifCondensed-BoldItalic",
+                 "DejaVuMathTexGyre"),
+        stringsAsFactors = FALSE
+      ) )
+
+      private$fontdir <- "dejavu-fonts/ttf"
+      private$package <- "fontHeavy"
+    }
+  )
+)
+
+
+
+#' @export
+#' @rdname font_desc
+#' @details
+#'
+#' \code{font_desc_dejavu} return details about Liberation
+#' font families and styles.
+font_desc_liberation <- R6Class(
+  "font_desc_liberation",
+  inherit = font_desc,
+  public = list(
+    initialize = function() {
+
+      self$set_data ( data.frame(
+        family = rep( c( "sans", "serif", "mono" ), each = 4 ),
+        style = rep(c( "regular", "bold", "italic", "bolditalic"), 3 ),
+        bold = rep(c(FALSE, TRUE, FALSE, TRUE), 3 ),
+        italic = rep(c(FALSE, FALSE, TRUE, TRUE), 3 ),
+        base = c( "LiberationSans-Regular", "LiberationSans-Bold", "LiberationSans-Italic", "LiberationSans-BoldItalic",
+                  "LiberationSerif-Regular", "LiberationSerif-Bold", "LiberationSerif-Italic", "LiberationSerif-BoldItalic",
+                  "LiberationMono-Regular", "LiberationMono-Bold", "LiberationMono-Italic", "LiberationMono-BoldItalic"),
+        stringsAsFactors = FALSE
+      ) )
+
+      private$fontdir <- "liberation-fonts"
+      private$package <- "fontHeavy"
+    }
+  )
+)
+
+
+# families <- sapply( font_dejavu_files, length )
+# names(families) <- gsub(pattern = "-", replacement = "", names(families))
+# families <- unlist( purrr::map2(names(families), families, function(x, y) rep(x, y)) )
+# dput(families)
+# style <- setNames( unlist( lapply( font_dejavu_files, names ) ), NULL )
+# dput(style)
+# bold <- c(F, T, F, T, F, F, T, F, T, F, T, F, T, F, T, F, T, F, T, F, T, F)
+# dput(bold)
+# italic <- c(F, F, T, T, F, F, F, T, T, F, F, T, T, F, F, T, T,F, F, T, T,F)
+# dput(italic)
+#
