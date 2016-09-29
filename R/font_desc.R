0594022707483af41962e722ace2fe1b71a80748
dgohel <david.gohel@ardata.fr>
2016-09-29 23:41:02 +0200

    add font_desc_html_dependency

--- R/font_desc.R
+++ R/font_desc.R
@@ -3,8 +3,9 @@
 #' @format an object that describes a font
 #' @description Summarize a font package.
 #' @field get_data get a data.frame describing a font
+#' @field get_id get font id
 #' @field get_families get a list to be used by a graphic device.
-#' @field set_data convenience function to be used when creating a
+#' @field set_data,set_id convenience function to be used when creating a
 #' font description object.
 #' @details
 #' \code{font_desc} is only there to provide common structure and behavior
@@ -25,6 +26,20 @@ font_desc <- R6Class(
     get_data = function(){
       private$content
     },
+    set_id = function(fontid){
+      private$id <- fontid
+      self
+    },
+    get_id = function(){
+      private$id
+    },
+    set_version = function(version){
+      private$version <- version
+      self
+    },
+    get_version = function(){
+      private$version
+    },
     set_data = function(df){
 
       stopifnot( "family" %in% names(df) )
@@ -37,6 +52,7 @@ font_desc <- R6Class(
       stopifnot( all( c("regular", "bold", "italic", "bolditalic") %in% df$style ) )
 
       private$content <- df
+      self
     },
 
     get_families = function() {
@@ -60,6 +76,8 @@ font_desc <- R6Class(
   private = list(
     content = NULL,
     fontdir = NULL,
+    id = NULL,
+    version = NULL,
     package = NULL
   )
 )
@@ -85,10 +103,11 @@ font_desc_bitstream_vera <- R6Class(
                   "VeraSe", "VeraSeBd", NA, NA,
                   "VeraMono", "VeraMoBd", "VeraMoIt", "VeraMoBI"),
         stringsAsFactors = FALSE
-      ) )
+      ) )$set_id("bitstream_vera")
 
       private$fontdir <- "bitstream-vera-fonts"
       private$package <- "fontBitstreamVera"
+      self$set_version(packageVersion("fontBitstreamVera"))
     }
   )
 )
@@ -130,10 +149,11 @@ font_desc_dejavu <- R6Class(
                  "DejaVuSerifCondensed-Bold", "DejaVuSerifCondensed-Italic", "DejaVuSerifCondensed-BoldItalic",
                  "DejaVuMathTexGyre"),
         stringsAsFactors = FALSE
-      ) )
+      ) )$set_id("dejavu")
 
       private$fontdir <- "dejavu-fonts/ttf"
       private$package <- "fontHeavy"
+      self$set_version(packageVersion("fontHeavy"))
     }
   )
 )
@@ -161,10 +181,11 @@ font_desc_liberation <- R6Class(
                   "LiberationSerif-Regular", "LiberationSerif-Bold", "LiberationSerif-Italic", "LiberationSerif-BoldItalic",
                   "LiberationMono-Regular", "LiberationMono-Bold", "LiberationMono-Italic", "LiberationMono-BoldItalic"),
         stringsAsFactors = FALSE
-      ) )
+      ) )$set_id("liberation")
 
       private$fontdir <- "liberation-fonts"
       private$package <- "fontHeavy"
+      self$set_version(packageVersion("fontHeavy"))
     }
   )
 )
@@ -181,3 +202,72 @@ font_desc_liberation <- R6Class(
 # italic <- c(F, F, T, T, F, F, F, T, T, F, F, T, T, F, F, T, T,F, F, T, T,F)
 # dput(italic)
 #
+
+#' @import purrr
+#' @title include fonts as as CSS dependency
+#' @description include fonts for families \code{sans}, \code{serif}, \code{mono}
+#' as as CSS dependency.
+#' @param font_description a \code{font_desc} object
+#' @export
+font_desc_html_dependency <- function(font_description =
+                                     font_desc_bitstream_vera$new() ) {
+  if (!requireNamespace("htmltools")) {
+    stop("htmltools is not installed", call. = FALSE)
+  }
+
+  css_dir <- file.path(tempdir(), "fontquiver")
+  if (!dir.exists(css_dir)) {
+    dir.create(css_dir)
+  }
+  id <- font_description$get_id()
+  families_ <- c("sans", "serif", "mono")
+  styles_ <- c("regular", "bold", "italic", "bolditalic")
+  ttf_files <- setNames( unlist( font_description$get_families() ), NULL )
+  fd_df <- font_description$get_data()
+  fd_df <- fd_df[fd_df$family %in% families_, ]
+  fd_df <- fd_df[fd_df$style %in% styles_, ]
+  order_ <- order(factor(fd_df$family, levels = families_ ) ,
+        factor(fd_df$style, levels = styles_ ) )
+  fd_df <- fd_df[order_, ]
+
+  fd_df$file_ttf <- ttf_files
+  fd_df$file_woff <- gsub( pattern = "\\.ttf$", replacement = ".woff", x = ttf_files )
+  fd_df$family_id <- paste( rep(id, nrow(fd_df) ), fd_df$family, sep = "-")
+
+  args <- list( id = fd_df$family_id,
+    file = fd_df$base,
+    bold = fd_df$bold, italic = fd_df$italic )
+
+  css <- pmap_chr(args, get_css_font_face)
+  css_file <- tempfile(id, css_dir, fileext = ".css")
+  writeLines(css, css_file, useBytes = TRUE)
+
+  fd_df <- fd_df[ !is.na( fd_df$base ), ]
+  to <- file.path(css_dir, basename(fd_df$file_ttf))
+  file.copy(fd_df$file_ttf, to)
+  to <- file.path(css_dir, basename(fd_df$file_woff))
+  file.copy(fd_df$file_woff, to)
+
+  htmltools::htmlDependency(
+    name = id,
+    version = packageVersion("fontquiver"),
+    src = css_dir,
+    stylesheet = basename(css_file)
+  )
+}
+
+get_css_font_face <- function(id, file, bold, italic) {
+  if( is.na(file) ) return ("")
+  paste( c(
+    "@font-face {",
+    sprintf("font-family: '%s';", id),
+    ifelse(italic, "font-style: italic;", ""),
+    ifelse(bold, "font-weight: bold;", ""),
+    sprintf("src: url('%s.ttf') format('truetype'), url('%s.woff') format('woff');", file, file),
+    "}"
+  ), collapse = "\n")
+}
+
+
+
+
